6a0627457fbec0110946382730a30029176203b7
nopnop2002 <hau26280@tree.odn.ne.jp>
2017-09-24 11:13:38 +0900

    20170924

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo ./nokia r
-sudo ./nokia +1 ABCDEFGHIJ
-sudo ./nokia +2 abcdefghij
-sudo ./nokia +3 1234567890
-sudo ./nokia +d ABCDEFGHIJ
-sudo ./nokia +e abcdefghij
-sudo ./nokia +f 1234567890
+./nokia r
+./nokia +1 ABCDEFGHIJ
+./nokia +2 abcdefghij
+./nokia +3 1234567890
+./nokia +d ABCDEFGHIJ
+./nokia +e abcdefghij
+./nokia +f 1234567890
 sudo ./nokia s
 
 i=1
@@ -14,7 +14,7 @@ while [ $i -le 10 ]
 do 
   echo -n "contrast="
   echo $contrast
-  sudo ./nokia c $contrast
+  ./nokia c $contrast
   sudo ./nokia s
   contrast=$(expr $contrast + 1)
   i=$(expr $i + 1)
