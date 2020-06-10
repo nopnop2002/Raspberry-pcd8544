38d6e3cf1fc3325abeed1146eecea8ef77492f03
nopnop2002 <hau26280@tree.odn.ne.jp>
2020-06-10 22:11:58 +0900

    Update me.sh

--- me.sh
+++ me.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-set -x
+#set -x
 
 argc=$#
 if [ $argc -eq 1 ]; then
@@ -17,21 +17,28 @@ IFS=' '
 set -- $VERSION_CODENAME
 ./nokia +2 "$1"
 
-ipaddr=`sudo ifconfig eth0 |grep "inet" |awk {'print $2'} |cut -f2 -d:`
-if [ -z ${ipaddr} ]; then
-  ./nokia +c "No eth0"
-else
+eth0=`sudo ifconfig -a | grep -c eth0`
+if [ $eth0 == "1" ]; then
+  ipaddr=`sudo ifconfig eth0 |grep "inet" |awk {'print $2'} |cut -f2 -d:`
   ./nokia +c "Ethernet"
   ./nokia +4 $ipaddr
+else
+  ./nokia +c "No eth0"
 fi
 
-ipaddr=`sudo ifconfig wlan0 |grep "inet" |awk {'print $2'} |cut -f2 -d:`
-if [ -z ${ipaddr} ]; then
+ipaddr=""
+wlan=`sudo ifconfig -a | grep -c wlan0`
+if [ $wlan == "1" ]; then
+  ipaddr=`sudo ifconfig wlan0 |grep "inet" |awk {'print $2'} |cut -f2 -d:`
+fi
+
+wlan=`sudo ifconfig -a | grep -c wlan1`
+if [ $wlan == "1" ]; then
   ipaddr=`sudo ifconfig wlan1 |grep "inet" |awk {'print $2'} |cut -f2 -d:`
 fi
 
 if [ -z ${ipaddr} ]; then
-  ./nokia +e "No wlan1"
+  ./nokia +e "No wlan"
 else
   ./nokia +e "WiFi"
   ./nokia +6 $ipaddr
